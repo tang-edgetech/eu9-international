0328a8a4f577cabc607da75706975e308e1cdfc1
tang-edgetech <tang.edgetech@gmail.com>
2025-06-16 09:50:38 +0800

    Upload new plugins and backup all media

--- wp-content/plugins/header-footer-elementor/build/main.asset.php
+++ wp-content/plugins/header-footer-elementor/build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '4e562a8f7675554263c3');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '8baa5ea1ac6992e21cf2');
